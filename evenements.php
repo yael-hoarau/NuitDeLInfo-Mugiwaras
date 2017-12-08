3e71e6ae5117723a776fa9b43510903bba89e512
Kévin <kevin.falco@etu.univ-amu.fr>
2017-12-08 07:23:28 +0100

    Modification du css

--- evenements.php
+++ evenements.php
@@ -1,9 +1,10 @@
 <?php
 require 'structPage.inc.php';
+require 'bd.php';
 start_page();
 ?>
     <div class="article">
-        <h1> Titre Event 1</h1>
+        <h1><?php ?></h1>
         <h2> Du 2 juin au 3 juin à La-Brioche-En-Oeuvre, PACA</h2>
         Ceci est l'event 1. Il constituera à faire toute la nuit. Cependant de l'alcool sera présent. Il faudra donc rester vigilant dans la zone autour de l'événement. Et si vous souhaitez participer, venez sur mon site web.
         <p class="Signature">Posté par La personne gérant l'event 2.</p>
